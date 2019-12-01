ca176bfe71d80ac1a3fbce0ad1fb15faebb02a0f
Markham <quinesquines@gmail.com>
2019-12-01 13:23:44 -0800

    Add ANSI colors

---
+++ util/shared.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+############
+# COLORS
+############
+COLOR_Cyan () { printf "\u001b[36m" ; }
+COLOR_Black () { printf "\u001b[30m" ; }
+COLOR_Red () { printf "\u001b[31m" ; }
+COLOR_Green () { printf "\u001b[32m" ; }
+COLOR_Yellow () { printf "\u001b[33m" ; }
+COLOR_Blue () { printf "\u001b[34m" ; }
+COLOR_Magenta () { printf "\u001b[35m" ; }
+COLOR_Cyan () { printf "\u001b[36m" ; }
+COLOR_White () { printf "\u001b[37m" ; }
+COLOR_Reset () { printf "\u001b[0m" ; }
+COLOR_BR_Black () { printf "\u001b[30;1m" ; }
+COLOR_BR_Red () { printf "\u001b[31;1m" ; }
+COLOR_BR_Green () { printf "\u001b[32;1m" ; }
+COLOR_BR_Yellow () { printf "\u001b[33;1m" ; }
+COLOR_BR_Blue () { printf "\u001b[34;1m" ; }
+COLOR_BR_Magenta () { printf "\u001b[35;1m" ; }
+COLOR_BR_Cyan () { printf "\u001b[36;1m" ; }
+COLOR_BR_White () { printf "\u001b[37;1m" ; }
