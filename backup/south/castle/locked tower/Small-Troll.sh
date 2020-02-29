3d40fea9b7ae114d4540f866234129348040d1b2
Markham <quinesquines@gmail.com>
2020-02-29 12:49:53 -0800

    Save troll instructions to a file in bag

--- backup/south/castle/locked tower/Small-Troll.sh
+++ backup/south/castle/locked tower/Small-Troll.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SAVE_FILE=troll-instructions.txt
+INSTRUCTIONS="${SPEECH}There is a certain path into the swamp with sure footing. You must follow this path to its termination and carry the head of the gargoyle with you. You must transmute the gargoyle's head into a $(alt magic-seed.sh). Then you must $(alt activate) the seed in that location."
+
 function tutorial () {
 	speak <<-EOF
 	$(art small-troll.txt)
@@ -7,7 +10,7 @@ function tutorial () {
 
 	In return for freeing me, I will tell you a tremendous secret: the only way to reach the mountain is to create a portal by magic. To do so, you must obtain the head of a gargoyle. But that is not all!
 
-	There is a certain path into the swamp with sure footing. You must follow this path to its termination and carry the head of the gargoyle with you. You must transmute the gargoyle's head into a $(alt magic-seed.sh). Then you must $(alt activate) the seed in that location.
+	${INSTRUCTIONS}
 
 	See what happens!
 
@@ -16,8 +19,12 @@ function tutorial () {
 }
 
 function reward () {
+	echo "$INSTRUCTIONS" > ~/bag/$SAVE_FILE
 	wrap <<-EOF
-	${SPEECH}Running like a rabbit, the little troll dashes past you and down the stairs and out of sight.
+	${SPEECH}
+	Running like a rabbit, the little troll dashes past you and down the stairs and out of sight.
+
+	You take a moment to scribe the instructions he just uttered on a file named $(alt $SAVE_FILE) and deposit it into your $(alt bag). If you want to review its contents, just $(spell "cat ~/bag/$SAVE_FILE")
 	EOF
 	rm "$0"
 }
