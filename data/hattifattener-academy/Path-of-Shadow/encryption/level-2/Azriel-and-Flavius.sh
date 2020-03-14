f73a183302d2943abea7177827c5454e34d978f7
Markham <quinesquines@gmail.com>
2020-03-13 20:02:25 -0700

    Spelling fixes on Azriel and Flavius

--- data/hattifattener-academy/Path-of-Shadow/encryption/level-2/Azriel-and-Flavius.sh
+++ data/hattifattener-academy/Path-of-Shadow/encryption/level-2/Azriel-and-Flavius.sh
@@ -32,7 +32,7 @@ tutorial () {
 
 	${AZRIEL}Oh, I'm sure everyone does that from time to time.
 
-	${FLAVIUS}But assymetric-key cryptography provides a solution to both of these problems. Under this scheme, I create $(alt two)${FLAVIUS} keys: a private one, which I keep secret, and a public one, which I can share with everyone.
+	${FLAVIUS}But asymmetric-key cryptography provides a solution to both of these problems. Under this scheme, I create $(alt two)${FLAVIUS} keys: a private one, which I keep secret, and a public one, which I can share with everyone.
 
 	I send the public key to all my friends and family, including Azriel. I can even share the public key with strangers! Azriel will use my public key to encrypt the file for me. Once it is encrypted this way, only my private key will unlock it. No one else, not even Azriel, can decrypt the file.
 
@@ -76,7 +76,7 @@ tutorial () {
 	* This was easier than encrypting a file. You didn't have to specify which key to use because $(spell gpg)${AZRIEL} automatically tries all of the keys on your keyring!
 	* This will write the decrypted content to your terminal, so be sure to use a redirect if you want to write it to a file. (This is very important if the file is large.)
 
-	${FLAVIUS}Oh, but that was a lot of materials. Be sure to use $(spell lessons)${FLAVIUS} to review all of these invocations. (You might want to pipe its output to $(spell grep)${FLAVIUS} and use $(spell grep)${FLAVIUS} to just search for lessons that include the word $(alt gpg)${FLAVIUS}.)
+	${FLAVIUS}Oh, but that was a lot of material. Be sure to use $(spell lessons)${FLAVIUS} to review all of these invocations. (You might want to pipe its output to $(spell grep)${FLAVIUS} and use $(spell grep)${FLAVIUS} to just search for lessons that include the word $(alt gpg)${FLAVIUS}.)
 
 	${FLAVIUS}You probably had better practice this just a bit. Try each of these tasks:
 
