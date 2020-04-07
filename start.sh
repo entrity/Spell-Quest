db14aa1ccfb82d8797ea59d473521e4248764e13
Markham Anderson <entrity@users.noreply.github.com>
2020-04-07 10:18:41 -0700

    Install wget and openssl and gpg in start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,16 @@ if >/dev/null which dpkg && >/dev/null which apt; then
 	>/dev/null which python3 || \
 	sudo apt install -y python || \
 	sudo apt install -y python3
+
+        >/dev/null which wget || \
+	>/dev/null which curl || \
+	sudo apt install -y wget
+
+	>/dev/null which openssl || \
+        sudo apt install -y openssl
+
+        >/dev/null which gpg || \
+        sudo apt install -y gpg
 fi
 
 __DIR__=$(dirname "$0")
